10e5ff0e960ca0ced09947ae1d4f5be2e930dcd0
Akash1507 <atakashsharma@gmail.com>
2020-01-10 02:22:02 +0530

    adding models to admin

--- memberships/admin.py
+++ memberships/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from memberships.models import Membership,UserMembership,Subscription
 
-# Register your models here.
+admin.site.register(Membership)
+admin.site.register(UserMembership)
+admin.site.register(Subscription)
