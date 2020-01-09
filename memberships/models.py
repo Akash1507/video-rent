20c3ea3f628c1ad883486dc082d1b8e7c59ee230
Akash1507 <atakashsharma@gmail.com>
2020-01-10 01:50:00 +0530

    membership, usermembership, subscription models created

--- memberships/models.py
+++ memberships/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+MEMBERSHIP_CHOICES = (
+    ('ENTERPRISE','ent'),
+    ('PROFESSIONAL','pro'),
+    ('FREE','free')
+)
+class Membership(models.Model):
+    slug = models.SlugField()
+    membership_type = models.CharField(choices=MEMBERSHIP_CHOICES,default="free",max_length=20)
+    price = models.IntegerField(default=15)
+    stripe_plan_id = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.membership_type
+
+
+class UserMembership(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    stripe_customer_id = models.CharField(max_length=50)
+    membership = models.OneToOneField(Membership,on_delete=models.CASCADE)
+
+class Subscription(models.Model):
+    user_membership = models.ForeignKey(UserMembership,on_delete=models.CASCADE)
+    stripe_subscription_id = models.CharField(max_length=40)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
